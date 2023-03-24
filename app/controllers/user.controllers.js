bae82be08c70d922ab9166ffb82237cbf67fac63
CristiancitoUwU <cdramirez792@misena.edu.co>
2023-03-24 17:02:39 -0500

    ya se puede agregar,eliminar actulizar, mostrar y buscar usuarios y quedan agrgados en la base de datos, tambien nos dieron el documento super largo para hacer la documentacion del testing

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -3,9 +3,9 @@ import {pool} from "../config/database/db";
 export const findAllUser = async(req,res)=>{
         try {
             const [rows] = await pool.query("CALL spFindAllUser();");
-            res.json (rows);
+            res.json(rows[0]);
         } catch (error) {
-            console.error("error en la consulta");
+            console.error(error);
             
         }
 }
@@ -23,12 +23,43 @@ export const findUser =async(req,res)=>{
 }
 export const insertUser = async(req,res)=>{
      const name = req.body.name;
-     const result = await pool.query(`CALL spInsertUser(${name});`);
-     res.json(result);
+     try {
+        const result = await pool.query(`CALL spInsertUser('${name}');`);
+        if (result[0].affectedRows !=0) {
+            res.json(result);
+        }else
+            res.json({"msg":"no actualizo"})
+        
+
+     } catch (error) {
+         console.error("ha ocurrido un error" + error);
+     }
+    };
+    
+export const deleteUser =async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const result = await pool.query(`CALL spDeleteUser(${id})`);
+    if(result[0].affectedRows==1)
+    res.json(result)
+    else
+    res.json({"error":"perra"})
+    } catch (error) {
+        console.error("ha ocurrido un error" + error)
+    }
 }
-export const deleteUser =(req,res)=>{
+export const updateUser =async(req,res)=>{
+    const id = req.body.id;
+    const name = req.body.name;
+    try {
+        const result = await pool.query(`CALL spUpdateUser(${id}, '${name}')`);
+        if(result[0].affectedRows !=0)
+            res.json(result);
+        else
+        res.json({"error": "no actulizo"})
+    } catch (error) {
+        console.error(error);
+    }
 
 }
-export const updateUser =(req,res)=>{
 
-}
